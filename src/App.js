3257aa94b2e537f3b5e113eb46ffed506499ed13
stefan504 <63974903+stefan504@users.noreply.github.com>
2021-07-26 08:59:51 +0200

    read

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const API_KEY = 'ea7cb1c3a50b1195881e3798e5a0c098';
+
+	useEffect(() => {
+		const fetchData = () => {
+			axios
+				.get(
+					`api.openweathermap.org/data/2.5/weather?q=belgrade&appid=${API_KEY}`
+				)
+				.then((res) => {
+					console.log(res);
+				});
+		};
+		fetchData();
+	});
+
+	return <div className="App">yo</div>;
 }
 
 export default App;
